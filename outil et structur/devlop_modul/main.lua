f88985a5ab3ba7d7a6e5ba57bf35915ded83e365
arperben <ben.gamer027@gmail.com>
2026-05-11 17:25:28 +0200

    Add files via upload

---
+++ outil et structur/devlop_modul/main.lua
@@ -0,0 +1,94 @@
+-- main_test
+
+local modul=require("modul/modul")
+
+modul.objet.center = true
+
+lo = {sol = modul.objet:create{
+   x = -500,
+   y = -50,
+   height = 50,
+   width = 1000
+}}
+
+temoin = modul.objet:create{
+   x = -100,
+   y = -100,
+   height = 100,
+   width = 100
+}
+
+perso = modul.objet:create{
+   x = 0,
+   y = -100,
+   rayon = 5
+}
+
+perso2 = modul.objet:create{
+   x = -300,
+   y = -300,
+   rayon = 10,
+   r = 0,
+   g = 0,
+   b = 255
+}
+
+modul.dt.save_txt("save.txt", {perso2, perso})
+
+function love.keypressed(key, scancode, isrepeat)
+   if key == 'escape' then
+      love.window.close()
+   elseif key == 'up' then
+      perso:saut()
+   --elseif key == 'down' then
+      --perso:mouv(0,1)
+   elseif key == 'right' then
+      perso:mouv(1,0)
+   elseif key == 'left' then
+      perso:mouv(-1,0)
+
+   elseif key == 'z' then
+      perso2:mouv(0,-10,false)
+   elseif key == 's' then
+      perso2:mouv(0,10,false)
+   elseif key == 'd' then
+      perso2:mouv(10,0,false)
+   elseif key == 'q' then
+      perso2:mouv(-10,0,false)
+   end
+end
+
+function love.load()
+   love.window.setMode(800, 600, {resizable=true})
+   love.window.maximize()
+   --t = modul.dt.load("save.txt")
+   --print(modul.show(t))
+end
+
+function love.update()
+   modul.update()
+   temoin.x = perso2.x
+   temoin.y = perso2.y
+   if temoin:mouseIsPass() then
+      temoin.r = 255
+   else
+      temoin.r = 0
+   end
+   if perso2:collision_obj(temoin) then
+      perso2.b = 0
+   else
+      perso2.b = 255
+   end
+   perso:updateGravityCollision(lo)
+end
+
+function love.draw()
+   lo.sol:draw('rectangle', 'line')
+   perso:draw('circle')
+   perso2:draw('circle')
+   temoin:draw()
+end
+
+function love.quit()
+   print('bye')
+end
---
+++ outil et structur/devlop_modul/modul/data.lua
@@ -0,0 +1,83 @@
+---------
+-- data
+-- v0.0.0
+---------
+
+local TSerial = require("modul/TSerial")
+local luaP = require("modul/lua+")
+
+local dt = {}
+
+function dt.save(name, data)
+
+    -- on vérifie que name et data son du bon type
+    luaP.check(data, "table", "data doit être de type table")
+    luaP.check(name, "string", "nameFichier doit être de type string")
+
+    local temp = TSerial.pack(data) -- la fonction permet de transformer data en class data
+
+    temp = love.data.compress("string", "zlib", temp, 9) -- on compress
+    temp = love.data.encode("string", "base64", temp)
+
+    local sucess, message = love.filesystem.write(name, temp)
+
+    if not sucess then
+        error("fichier non crée/modifier : ".. message)
+    end
+end
+
+function dt.load(name)
+
+    luaP.check(name, "string", "nameFichier doit être de type string")
+
+    if love.filesystem.getInfo(name) then -- on vérifie si le fichier existe
+
+        data = love.data.decode("string", "base64", love.filesystem.read(name)) -- on decode puis decompresse
+        data = love.data.decompress("string", "zlib", data)
+
+        data = TSerial.unpack(data) -- on unpack les donnée pour pouvoir les utilisers
+
+    else
+        return false
+    end
+
+    return data
+end
+
+function dt.save_txt(name, data)
+
+    -- on vérifie que name est du bon type
+
+    if type(name) ~= "string" then
+        error("nameFichier doit être de type string")
+    end
+    if type(data) == "table" then
+        data = luaP.show(data)
+    elseif type(data) ~= "string" then
+        error("data doit être de type string ou table")
+    end
+
+    local sucess, message = love.filesystem.write(name, data)
+
+    if not sucess then
+        error("fichier non crée/modifier : ".. message)
+    end
+end
+
+function dt.load_txt(name)
+
+    if type(name) ~= "string" then
+        error("nameFichier doit être de type string")
+    end
+
+    if love.filesystem.getInfo(name) then -- on vérifie si le fichier existe
+        data = love.filesystem.read(name) -- on récup les donnée
+
+    else
+        return ""
+    end
+
+    return data
+end
+
+return dt
